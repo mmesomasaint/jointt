9299d20ad766e68d81139e4ab175b7b1d72776f4
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-19 21:12:03 +0100

    Feat: Create the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
     <main className="bg-white">
       <header className="bg-white px-4">
         <div className="py-4 border-b border-b-zinc-700/50 flex justify-end items-center gap-5">
-          <button className="rounded-xl px-5 py-3 leading-none bg-white border-blue-600/50 text-blue-600">
+          <button className="rounded-xl px-5 py-3 leading-none bg-white border border-blue-600/50 text-blue-600">
             Sign in
           </button>
-          <button className="rounded-xl px-5 py-3 leading-none bg-white border-blue-600/50 text-blue-600">
+          <button className="rounded-xl px-5 py-3 leading-none bg-white border border-blue-600/50 bg-blue-600 text-white">
             Sign up
           </button>
         </div>
