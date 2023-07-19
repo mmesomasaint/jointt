580a254d8712451ad66f3328d382b3a913f14c88
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-19 21:12:20 +0100

    Fix: Format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,18 @@
 export default function Home() {
   return (
-    <main className="bg-white">
-      <header className="bg-white px-4">
-        <div className="py-4 border-b border-b-zinc-700/50 flex justify-end items-center gap-5">
-          <button className="rounded-xl px-5 py-3 leading-none bg-white border border-blue-600/50 text-blue-600">
+    <main className='bg-white'>
+      <header className='bg-white px-4'>
+        <div className='py-4 border-b border-b-zinc-700/50 flex justify-end items-center gap-5'>
+          <button className='rounded-xl px-5 py-3 leading-none bg-white border border-blue-600/50 text-blue-600'>
             Sign in
           </button>
-          <button className="rounded-xl px-5 py-3 leading-none bg-white border border-blue-600/50 bg-blue-600 text-white">
+          <button className='rounded-xl px-5 py-3 leading-none bg-white border border-blue-600/50 bg-blue-600 text-white'>
             Sign up
           </button>
         </div>
       </header>
-      <section>
-
-      </section>
-      <footer>
-
-      </footer>
+      <section></section>
+      <footer></footer>
     </main>
   )
 }
