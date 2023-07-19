c10b45b7fda9b045dc0a242b669fbb32193ddb4c
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-19 22:18:25 +0100

    Fix: Format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router"
+import { useRouter } from 'next/router'
 
 export default function Home() {
   const router = useRouter()
@@ -7,10 +7,16 @@ export default function Home() {
     <main className='bg-white'>
       <header className='bg-white px-4'>
         <div className='py-4 border-b border-b-zinc-700/50 flex justify-end items-center gap-5'>
-          <button className='rounded-xl px-5 py-3 leading-none bg-white border border-blue-600/50 text-blue-600' onClick={() => router.push('/auth/signin')}>
+          <button
+            className='rounded-xl px-5 py-3 leading-none bg-white border border-blue-600/50 text-blue-600'
+            onClick={() => router.push('/auth/signin')}
+          >
             Sign in
           </button>
-          <button className='rounded-xl px-5 py-3 leading-none border border-blue-600/50 bg-blue-600 text-white' onClick={() => router.push('/auth/signup')}>
+          <button
+            className='rounded-xl px-5 py-3 leading-none border border-blue-600/50 bg-blue-600 text-white'
+            onClick={() => router.push('/auth/signup')}
+          >
             Sign up
           </button>
         </div>
