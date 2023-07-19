6c3c0f6eeda404adfbd9f8dc4a4ca337014daf77
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-19 20:50:01 +0100

    Feat: Organise env.local

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    firebaseAPIKey: 'AIzaSyBxPa-pK7mR_FdPXrphY7rv_pTDgr4ODMY',
-    firebaseAuthDomain: 'jointt-e9963.firebaseapp.com',
-    firebaseProjectId: 'jointt-e9963',
-    firebaseStorageBucket: 'jointt-e9963.appspot.com',
-    firebaseMessagingSenderId: '1025883422247',
-    firebaseAppId: '1:1025883422247:web:2fd53816df28204df4d802',
-    firebaseMeasurementId: 'G-1ZVXREJ965',
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
