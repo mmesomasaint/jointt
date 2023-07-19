bdc8ac7fa8bce3a73bb41004bc8f65e81d77f906
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-19 19:51:55 +0100

    Feat: Modify env file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    firebaseAPIKey: 'AIzaSyBxPa-pK7mR_FdPXrphY7rv_pTDgr4ODMY',
+    firebaseAuthDomain: 'jointt-e9963.firebaseapp.com',
+    firebaseProjectId: 'jointt-e9963',
+    firebaseStorageBucket: 'jointt-e9963.appspot.com',
+    firebaseMessagingSenderId: '1025883422247',
+    firebaseAppId: '1:1025883422247:web:2fd53816df28204df4d802',
+    firebaseMeasurementId: 'G-1ZVXREJ965',
+  },
+}
 
 module.exports = nextConfig
