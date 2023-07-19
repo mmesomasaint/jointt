447777bac0d579d4e56670b649aaf8047104b4ec
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-19 21:09:14 +0100

    Feat: Create the sign up and sign in buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 export default function Home() {
   return (
     <main className="bg-white">
-      <header>
-
+      <header className="bg-white px-4">
+        <div className="py-4 border-b border-b-zinc-700/50 flex justify-end items-center gap-5">
+          <button className="rounded-xl px-5 py-3 leading-none bg-white border-blue-600/50 text-blue-600">
+            Sign in
+          </button>
+          <button className="rounded-xl px-5 py-3 leading-none bg-white border-blue-600/50 text-blue-600">
+            Sign up
+          </button>
+        </div>
       </header>
       <section>
 
