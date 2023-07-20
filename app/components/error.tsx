0686233cc20cb584b18c774086643f572ffa988f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 07:15:38 +0100

    Fix: Adjust error display

--- app/components/error.tsx
+++ app/components/error.tsx
@@ -14,7 +14,7 @@ const ErrorMessage = ({ message, onClose }: ErrorMessageType) => {
   }
 
   return isVisible ? (
-    <div className='fixed top-0 left-0 w-full flex justify-center px-4 py-2 z-50'>
+    <div className='w-full flex justify-center px-4 py-2 z-50 transform duration-100'>
       <div className='bg-red-500 text-white px-4 py-2 rounded shadow-md flex items-center'>
         <span className='mr-2'>{message}</span>
         <button className='text-white font-bold' onClick={handleClose}>
@@ -38,11 +38,11 @@ const useErrorHandler = () => {
     setErrors((prevErrors) => [...prevErrors, msg])
 
   const Errors = () => (
-    <>
+    <div className='fixed top-0 left-0 flex flex-col gap-3 items-center justify-evenly'>
       {errors.map((msg: string, id: number) => (
         <ErrorMessage message={msg} onClose={() => closeError(id)} />
       ))}
-    </>
+    </div>
   )
   return { errors, addError, Errors }
 }
