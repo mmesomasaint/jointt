cef736689528815c2a5886d07089672b7b98b2a6
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 00:01:43 +0100

    Fix: Format with prettier

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -1,21 +1,21 @@
-import Text from "@/app/components/text"
-import Tab from "../components/tab"
+import Text from '@/app/components/text'
+import Tab from '../components/tab'
 
 export default function Events() {
   const handleTabs = () => {}
 
   return (
-    <div className="flex flex-col justify-start gap-0">
-      <div className="p-3"><Text size='BOLD'>Events</Text>
-      <div className="mt-24 flex justify-evenly items-center gap-5">
-        <Tab onClick={handleTabs}>Active</Tab>
-        <Tab onClick={handleTabs}>Pending</Tab>
-        <Tab onClick={handleTabs}>Records</Tab>
+    <div className='flex flex-col justify-start gap-0'>
+      <div className='p-3'>
+        <Text size='BOLD'>Events</Text>
+        <div className='mt-24 flex justify-evenly items-center gap-5'>
+          <Tab onClick={handleTabs}>Active</Tab>
+          <Tab onClick={handleTabs}>Pending</Tab>
+          <Tab onClick={handleTabs}>Records</Tab>
+        </div>
+        <hr />
       </div>
-      <hr/></div>
-      <div className="flex-grow">
-        This is the display...
-      </div>
+      <div className='flex-grow'>This is the display...</div>
     </div>
   )
-} 
+}
