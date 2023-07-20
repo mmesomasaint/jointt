575d3b19bbc2f8a8877c1d302b04d77702bebab7
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 07:52:39 +0100

    Feat: Design the context for auth

---
+++ app/auth/authcontext.tsx
@@ -0,0 +1,41 @@
+import {createContext, useContext, useState, useEffect} from 'react';
+import {
+    onAuthStateChanged,
+    getAuth,
+    User
+} from 'firebase/auth';
+import firebase_app from '@/app/firebase';
+
+const auth = getAuth(firebase_app);
+
+export const AuthContext = createContext({});
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({
+    children,
+}: {
+  children: React.ReactNode
+}) => {
+    const [user, setUser] = useState<User|null>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user }}>
+            {loading ? <div>Loading...</div> : children}
+        </AuthContext.Provider>
+    );
+};
