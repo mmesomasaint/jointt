d0b1ad2c7af62efe8efb73cdd74cc6b02506fef1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 05:28:55 +0100

    Fix: Format with prettier

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -23,8 +23,13 @@ function Page() {
   }
   return (
     <>
-      <h1 className='mt-44 mb-12 text-4xl font-bold text-black/70 ml-5'>Sign up</h1>
-      <form onSubmit={handleForm} className='flex flex-col justify-start items-start gap-5 ml-5'>
+      <h1 className='mt-44 mb-12 text-4xl font-bold text-black/70 ml-5'>
+        Sign up
+      </h1>
+      <form
+        onSubmit={handleForm}
+        className='flex flex-col justify-start items-start gap-5 ml-5'
+      >
         <label htmlFor='email'>
           <p>Email</p>
           <input
@@ -49,7 +54,12 @@ function Page() {
             className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
           />
         </label>
-        <button type='submit' className='rounded-xl px-5 py-3 leading-none border border-blue-600/50 bg-blue-600 text-white'>Sign up</button>
+        <button
+          type='submit'
+          className='rounded-xl px-5 py-3 leading-none border border-blue-600/50 bg-blue-600 text-white'
+        >
+          Sign up
+        </button>
       </form>
     </>
   )
