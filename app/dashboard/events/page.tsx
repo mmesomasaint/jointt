fe6e9817c95b270cf3c468e7de1e5e466afb62f1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 23:58:52 +0100

    Fix: Design the display

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -1,12 +1,21 @@
 import Text from "@/app/components/text"
+import Tab from "../components/tab"
 
 export default function Events() {
+  const handleTabs = () => {}
+
   return (
-    <div>
-      <Text size='BOLD'>Events</Text>
+    <div className="flex flex-col justify-start gap-0">
+      <div className="p-3"><Text size='BOLD'>Events</Text>
       <div className="mt-24 flex justify-evenly items-center gap-5">
+        <Tab onClick={handleTabs}>Active</Tab>
+        <Tab onClick={handleTabs}>Pending</Tab>
+        <Tab onClick={handleTabs}>Records</Tab>
       </div>
-      <hr/>
+      <hr/></div>
+      <div className="flex-grow">
+        This is the display...
+      </div>
     </div>
   )
-}
+} 
