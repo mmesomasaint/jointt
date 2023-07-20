9094af146ae42389d4a4e32e35eaea376677ce5e
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 20:58:18 +0100

    Fix: Format with prettier

--- app/dashboard/components/avatar.tsx
+++ app/dashboard/components/avatar.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-export default function Avatar({src}: {src: string | null}) {
+export default function Avatar({ src }: { src: string | null }) {
   return (
-    <Image src={src === null ? 'defaultpath' : src} width={350} height={350} alt="Profile Image" className="rouneded-full border border-black/50" />
+    <Image
+      src={src === null ? 'defaultpath' : src}
+      width={350}
+      height={350}
+      alt='Profile Image'
+      className='rouneded-full border border-black/50'
+    />
   )
 }
