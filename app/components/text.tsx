48a8c988c0885fe8534df0bc2fe0609424ceb5c4
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 21:37:09 +0100

    Fix: Format with prettier

--- app/components/text.tsx
+++ app/components/text.tsx
@@ -1,11 +1,21 @@
-export default function Text({size, children}: {size: "BOLD" | "SEMIBOLD" | "MEDIUM" | "SMALL", children: React.ReactNode}) {
+export default function Text({
+  size,
+  children,
+}: {
+  size: 'BOLD' | 'SEMIBOLD' | 'MEDIUM' | 'SMALL'
+  children: React.ReactNode
+}) {
   const BOLD = 'font-bold text-5xl'
   const SEMIBOLD = 'font-semibold text-3xl'
   const MEDIUM = 'font-medium text-xl'
   const SMALL = 'font-base text-lg'
 
   return (
-    <div className={`${size === 'BOLD' && BOLD} ${size === 'SEMIBOLD' && SEMIBOLD}${size === 'MEDIUM' && MEDIUM}${size === 'SMALL' && SMALL}`}>
+    <div
+      className={`${size === 'BOLD' && BOLD} ${
+        size === 'SEMIBOLD' && SEMIBOLD
+      }${size === 'MEDIUM' && MEDIUM}${size === 'SMALL' && SMALL}`}
+    >
       {children}
     </div>
   )
