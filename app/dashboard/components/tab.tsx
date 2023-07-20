3e702a129b581c30bb86aeb3d44777437d8b2a45
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 23:03:38 +0100

    Fix: Format with prettier

--- app/dashboard/components/tab.tsx
+++ app/dashboard/components/tab.tsx
@@ -1,6 +1,16 @@
-export default function Tab({onClick, children}: {onClick: () => void, children: React.ReactNode}) {
+export default function Tab({
+  onClick,
+  children,
+}: {
+  onClick: () => void
+  children: React.ReactNode
+}) {
   return (
-    <button type='button' className="p-3 border border-blue-600/30 text-black text-base font-medium shadow-xl" onClick={() => onClick()}>
+    <button
+      type='button'
+      className='p-3 border border-blue-600/30 text-black text-base font-medium shadow-xl'
+      onClick={() => onClick()}
+    >
       {children}
     </button>
   )
