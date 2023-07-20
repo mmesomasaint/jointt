9b760d08affdb635f64c952ae9fd967df919f88d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 22:57:40 +0100

    Fix: Design events

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -4,6 +4,9 @@ export default function Events() {
   return (
     <div>
       <Text size='BOLD'>Events</Text>
+      <div className="mt-24 flex justify-evenly items-center gap-5">
+      </div>
+      <hr/>
     </div>
   )
 }
