468c7935a65d89b106bdf8eb66b687735d81bfa9
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 20:50:58 +0100

    Feat: Design the Nav

---
+++ app/dashboard/components/nav.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <div className="flex flex-col justify-start items-start gap-5">
+      <NavLink href='/dashboard/events'>Events</NavLink>
+      <NavLink href='/dashboard/contracts'>Contracts</NavLink>
+      <NavLink href='/dashboard/career'>Career</NavLink>
+      <NavLink href='/dashboard/settings'>Settings</NavLink>
+    </div>
+  )
+}
+
+function NavLink({href, children}: {href: string, children: React.ReactNode}) {
+  return (
+    <div>
+      <Link href={href}>
+        {children}
+      </Link>
+    </div>
+  )
+}
