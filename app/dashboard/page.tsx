6c667ed0bed77bf5ae19e71276a5b382aea5033f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 09:12:00 +0100

    Fix: Handle typos

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,13 @@
-import { useState, useEffect } from "react"
-import { useRouter } from "next/navigation"
-import { useAuthContext } from "../auth/authcontext"
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useAuthContext } from '../auth/authcontext'
 
 export default function Dashboard() {
   const { user } = useAuthContext()
   const router = useRouter()
 
   useEffect(() => {
-      if (user == null) router.push("/")
+    if (user == null) router.push('/auth/signin')
   }, [user])
-  return (
-    <h1>Welcome to the dashboard</h1>
-  )
+  return <h1>Welcome to the dashboard</h1>
 }
