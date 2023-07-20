3c160a399baac663910c815c7307c12f83ee95e9
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 05:11:48 +0100

    Fix: Format with prettier

--- app/components/error.tsx
+++ app/components/error.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 type ErrorMessageType = {
   message: string
@@ -6,23 +6,23 @@ type ErrorMessageType = {
 }
 
 const ErrorMessage = ({ message, onClose }: ErrorMessageType) => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(true)
 
   const handleClose = () => {
-    setIsVisible(false);
-    onClose();
-  };
+    setIsVisible(false)
+    onClose()
+  }
 
   return isVisible ? (
-    <div className="fixed top-0 left-0 w-full flex justify-center px-4 py-2 z-50">
-      <div className="bg-red-500 text-white px-4 py-2 rounded shadow-md flex items-center">
-        <span className="mr-2">{message}</span>
-        <button className="text-white font-bold" onClick={handleClose}>
+    <div className='fixed top-0 left-0 w-full flex justify-center px-4 py-2 z-50'>
+      <div className='bg-red-500 text-white px-4 py-2 rounded shadow-md flex items-center'>
+        <span className='mr-2'>{message}</span>
+        <button className='text-white font-bold' onClick={handleClose}>
           X
         </button>
       </div>
     </div>
-  ) : null;
-};
+  ) : null
+}
 
-export default ErrorMessage;
+export default ErrorMessage
