15bcc5fc3b143bec7254788aa0aa9844091f9666
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 21:07:14 +0100

    Feat: Create the sidebar

---
+++ app/dashboard/components/sidebar.tsx
@@ -0,0 +1,11 @@
+import Avatar from "./avatar";
+import Nav from "./nav";
+
+export default function SideBar() {
+  return (
+    <div className="flex flex-col gap-24 justify-center items-center">
+    <Avatar src={null} />
+    <Nav />
+    </div>
+  )
+}
