8d842556b64d778b71ad986696f718417175c322
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 07:52:57 +0100

    Fix: Format with prettier

--- app/auth/authcontext.tsx
+++ app/auth/authcontext.tsx
@@ -1,41 +1,37 @@
-import {createContext, useContext, useState, useEffect} from 'react';
-import {
-    onAuthStateChanged,
-    getAuth,
-    User
-} from 'firebase/auth';
-import firebase_app from '@/app/firebase';
+import { createContext, useContext, useState, useEffect } from 'react'
+import { onAuthStateChanged, getAuth, User } from 'firebase/auth'
+import firebase_app from '@/app/firebase'
 
-const auth = getAuth(firebase_app);
+const auth = getAuth(firebase_app)
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext({})
 
-export const useAuthContext = () => useContext(AuthContext);
+export const useAuthContext = () => useContext(AuthContext)
 
 export const AuthContextProvider = ({
-    children,
+  children,
 }: {
   children: React.ReactNode
 }) => {
-    const [user, setUser] = useState<User|null>();
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<User | null>()
+  const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUser(user);
-            } else {
-                setUser(null);
-            }
-            setLoading(false);
-        });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user)
+      } else {
+        setUser(null)
+      }
+      setLoading(false)
+    })
 
-        return () => unsubscribe();
-    }, []);
+    return () => unsubscribe()
+  }, [])
 
-    return (
-        <AuthContext.Provider value={{ user }}>
-            {loading ? <div>Loading...</div> : children}
-        </AuthContext.Provider>
-    );
-};
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  )
+}
