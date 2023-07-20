7a1378fec1458eba68b4ab0170c626c2acc52ea0
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-20 21:37:00 +0100

    Feat: Design text

---
+++ app/components/text.tsx
@@ -0,0 +1,12 @@
+export default function Text({size, children}: {size: "BOLD" | "SEMIBOLD" | "MEDIUM" | "SMALL", children: React.ReactNode}) {
+  const BOLD = 'font-bold text-5xl'
+  const SEMIBOLD = 'font-semibold text-3xl'
+  const MEDIUM = 'font-medium text-xl'
+  const SMALL = 'font-base text-lg'
+
+  return (
+    <div className={`${size === 'BOLD' && BOLD} ${size === 'SEMIBOLD' && SEMIBOLD}${size === 'MEDIUM' && MEDIUM}${size === 'SMALL' && SMALL}`}>
+      {children}
+    </div>
+  )
+}
