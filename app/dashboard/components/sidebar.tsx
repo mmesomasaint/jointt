15ad6411f44bb2d9559694703e31c1d0539b7ea4
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 00:30:18 +0100

    Fix: Add more padding

--- app/dashboard/components/sidebar.tsx
+++ app/dashboard/components/sidebar.tsx
@@ -3,7 +3,7 @@ import Nav from './nav'
 
 export default function SideBar() {
   return (
-    <div className='flex flex-col gap-24 justify-center items-center'>
+    <div className='flex flex-col gap-24 justify-center items-center p-5'>
       <Avatar src={null} />
       <Nav />
     </div>
