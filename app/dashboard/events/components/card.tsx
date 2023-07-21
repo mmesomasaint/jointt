88031b5ab3554a4bd565eb8bcebd9ca831cd369f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 02:17:26 +0100

    Fix: Adjust shadow

--- app/dashboard/events/components/card.tsx
+++ app/dashboard/events/components/card.tsx
@@ -2,7 +2,7 @@ import Text from "@/app/components/text"
 
 export default function Card({name, type, cost, date}: {name:string, type:string, cost:number, date: Date}) {
   return (
-    <div className="w-full h-fit p-5 rounded-xl shadow-xl">
+    <div className="w-full h-fit p-5 rounded-xl shadow-md">
       <div className="w-full flex justify-between items-center gap-10">
         <Text size="SEMIBOLD">{name}</Text>
         <Text size="MEDIUM">N{cost}</Text>
