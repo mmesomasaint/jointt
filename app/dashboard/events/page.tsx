eadaf51bed02b9a3d5861ba02b579c0abd5a0fa9
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 01:03:22 +0100

    Fix: ADjust layout

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -7,9 +7,9 @@ export default function Events() {
 
   return (
     <div className='flex flex-col justify-start gap-0'>
-      <div className='p-3'>
+      <div className='p-3 pt-12'>
         <Text size='BOLD'>Events</Text>
-        <div className='mt-24 flex justify-start items-center gap-5'>
+        <div className='mt-16 flex justify-start items-center gap-5'>
           <Tab onClick={handleTabs}>Active</Tab>
           <Tab onClick={handleTabs}>Pending</Tab>
           <Tab onClick={handleTabs}>Records</Tab>
