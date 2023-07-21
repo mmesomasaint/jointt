39ad3cf887c08bed6741dc055bbbfe8845066604
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 19:54:12 +0100

    Fix: Make adjustments

--- app/dashboard/events/host/page.tsx
+++ app/dashboard/events/host/page.tsx
@@ -10,8 +10,8 @@ export default function Host() {
   return (
     <div className="p-3">
       <Text size="SMALL">Host An Event</Text>
-      <Text size='BOLD' className="text-center mt-20">CHOOSE EVENT TYPE</Text>
-        <div className="flex flex-col gap-4 justify-start items-start w-fit mx-auto mt-16"><label htmlFor='eventtype'>
+        <div className="flex flex-col gap-4 justify-start items-start w-fit mx-auto mt-16">
+      <Text size='BOLD' className="text-center mb-10">CHOOSE EVENT TYPE</Text><label htmlFor='eventtype' className="w-full">
           <Text size="SMALL">Event Name</Text>
           <input
             onChange={(e) => setEventName(e.target.value)}
@@ -21,10 +21,10 @@ export default function Host() {
             id='eventname'
             value={eventname}
             placeholder="Dora's Dedication"
-            className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
+            className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50 w-full'
           />
         </label>
-        <label htmlFor='eventtype'>
+        <label htmlFor='eventtype' className="w-full">
           <Text size="SMALL">Event Type</Text>
           <input
             onChange={(e) => setEventType(e.target.value)}
@@ -34,7 +34,7 @@ export default function Host() {
             id='eventtype'
             value={eventtype}
             placeholder='Party'
-            className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
+            className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50 w-full'
           />
         </label>
         <Button type='button'>Next</Button></div>
