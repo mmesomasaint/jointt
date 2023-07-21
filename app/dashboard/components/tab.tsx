fce3796bdf0e9225d2becd1611375d48060ebf74
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 01:30:06 +0100

    Fix: Adjust tab color

--- app/dashboard/components/tab.tsx
+++ app/dashboard/components/tab.tsx
@@ -8,7 +8,7 @@ export default function Tab({
   return (
     <button
       type='button'
-      className='text-black/50 text-xl font-medium leading-none'
+      className='text-black/70 text-xl font-medium leading-none'
       onClick={() => onClick()}
     >
       {children}
