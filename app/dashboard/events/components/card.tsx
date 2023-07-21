34deca5cab7719f54a705c20ef5ca671fd63c03c
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 02:22:36 +0100

    Fix: Edit card

--- app/dashboard/events/components/card.tsx
+++ app/dashboard/events/components/card.tsx
@@ -2,14 +2,14 @@ import Text from "@/app/components/text"
 
 export default function Card({name, type, cost, date}: {name:string, type:string, cost:number, date: Date}) {
   return (
-    <div className="w-full h-fit p-5 rounded-xl shadow-md">
-      <div className="w-full flex justify-between items-center gap-10">
+    <div className="w-full h-fit p-5 rounded-xl shadow-sm border border-gray-900/30">
+      <div className="w-full flex justify-between items-center gap-10 mb-5">
         <Text size="SEMIBOLD">{name}</Text>
         <Text size="MEDIUM">N{cost}</Text>
       </div>
       <div className="w-full flex justify-between items-center gap-10">
-        <Text size="SEMIBOLD">{type}</Text>
-        <Text size="MEDIUM">{date.toLocaleDateString()}</Text>
+        <Text size="MEDIUM">{type}</Text>
+        <Text size="SMALL">{date.toLocaleDateString()}</Text>
       </div>
     </div>
   )
