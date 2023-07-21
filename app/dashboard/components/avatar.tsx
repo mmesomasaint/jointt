ed9c70fe78087581d2af175b3d8bc14b284b1082
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 17:11:49 +0100

    Fix: Format with prettier

--- app/dashboard/components/avatar.tsx
+++ app/dashboard/components/avatar.tsx
@@ -4,15 +4,14 @@ import avatar from '../avatar.jpg'
 export default function Avatar({ src }: { src: string | null }) {
   return (
     <div className='relative w-[18vw] h-[18vw] rounded-full border border-black/50 shadow-md'>
-      
-    <Image
-      src={src === null ? avatar : src}
-      fill
-      priority
-      sizes='20vw'
-      alt='Profile Image'
-      className='rounded-full'
-    />
+      <Image
+        src={src === null ? avatar : src}
+        fill
+        priority
+        sizes='20vw'
+        alt='Profile Image'
+        className='rounded-full'
+      />
     </div>
   )
 }
