09c1152200480f05a950d523c8f736c856cc186f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 15:24:01 +0100

    Feat: Design modal

---
+++ app/dashboard/components/modal.tsx
@@ -0,0 +1,7 @@
+export default function Modal({children}: {children: React.ReactNode}) {
+  return (
+    <div className="absolute inset-0 bg-gray-900/30 flex justify-center items-center">
+      {children}
+    </div>
+  )
+}
