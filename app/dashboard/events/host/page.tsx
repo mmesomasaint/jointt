5e19cc3a8f9eb5b0edd7fda78ff483b92543d7de
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 19:43:37 +0100

    Fix: Make adjustments

--- app/dashboard/events/host/page.tsx
+++ app/dashboard/events/host/page.tsx
@@ -1,15 +1,17 @@
+'use client'
 import { useState } from "react";
 import Text from "@/app/components/text";
+import Button from "@/app/components/button";
 
 export default function Host() {
   const [eventname, setEventName] = useState('')
   const [eventtype, setEventType] = useState('')
 
   return (
-    <div>
-      <Text size="SMALL">Host an event</Text>
-      <Text size='BOLD'>CHOOSE EVENT TYPE</Text>
-        <label htmlFor='eventtype'>
+    <div className="p-3">
+      <Text size="SMALL">Host An Event</Text>
+      <Text size='BOLD' className="text-center">CHOOSE EVENT TYPE</Text>
+        <div className="flex flex-col gap-4 justify-start items-start w-fit mx-auto mt-16"><label htmlFor='eventtype'>
           <Text size="SMALL">Event Name</Text>
           <input
             onChange={(e) => setEventName(e.target.value)}
@@ -35,6 +37,7 @@ export default function Host() {
             className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
           />
         </label>
+        <Button type='button'>Next</Button></div>
     </div>
   )
 }
