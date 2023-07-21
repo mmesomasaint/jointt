65b96cd7368ad24197a87322fcdb92c870722455
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 01:28:40 +0100

    Fix: Format with prettier

--- app/dashboard/components/nocontent.tsx
+++ app/dashboard/components/nocontent.tsx
@@ -1,7 +1,9 @@
 export default function NoContent() {
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="text-gray-900/70 font-medium text-lg">Nothing to display here...</div>
+    <div className='w-full h-full flex justify-center items-center'>
+      <div className='text-gray-900/70 font-medium text-lg'>
+        Nothing to display here...
+      </div>
     </div>
   )
 }
