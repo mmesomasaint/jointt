2b0be9e60b5f59a2792e266443098bf7dbb6236a
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-21 20:56:39 +0100

    Feat: Add className to button and implement it

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -2,11 +2,13 @@ export default function Button({
   type,
   disabled,
   onClick,
+  className,
   children,
 }: {
   type: 'submit' | 'button' | 'reset' | undefined
   disabled?: boolean
   onClick?: () => void
+  className?: string
   children: React.ReactNode
 }) {
   return (
@@ -14,7 +16,7 @@ export default function Button({
       type={type}
       disabled={disabled}
       onClick={onClick}
-      className='rounded-xl px-5 py-3 leading-none border border-blue-600/50 bg-blue-600 text-white'
+      className={`rounded-xl px-5 py-3 leading-none border border-blue-600/50 bg-blue-600 text-white ${className}`}
     >
       {children}
     </button>
