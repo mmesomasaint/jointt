1c00075ed215cea69064874262fbb2510467efad
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-24 22:13:04 +0100

    Test: Focus on the 2nd stage

--- app/dashboard/events/host/page.tsx
+++ app/dashboard/events/host/page.tsx
@@ -5,7 +5,7 @@ import Button from '@/app/components/button'
 
 export default function Host() {
   const STAGES = [1, 2, 3, 4]
-  const [activeStage, setActiveStage] = useState(STAGES[0])
+  const [activeStage, setActiveStage] = useState(STAGES[1])
   const [eventName, setEventName] = useState('')
   const [eventType, setEventType] = useState('')
   const [contractorTypes, setContractorTypes] = useState<string[]>([
