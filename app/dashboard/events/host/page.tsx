2b80ccf105eda2221f10ddce6684360ddce1b9c2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-25 16:36:58 +0100

    Fix: Cleanup

--- app/dashboard/events/host/page.tsx
+++ app/dashboard/events/host/page.tsx
@@ -4,14 +4,18 @@ import Text from '@/app/components/text'
 import Button from '@/app/components/button'
 import { MdClose } from 'react-icons/md'
 
+type EventType = {
+  name: string
+  type: string
+  contractors?: string[]
+  totalCost?: number
+  setDate?: Date
+}
+
 export default function Host() {
   const STAGES = [1, 2, 3, 4]
-  const DEFAULTCONTRACTORS = ['Cook', 'MC', 'Singer', 'Dancers', 'Clowns']
   const [activeStage, setActiveStage] = useState(STAGES[2])
-  const [eventName, setEventName] = useState('')
-  const [eventType, setEventType] = useState('')
-  const [contractorTypes, setContractorTypes] =
-    useState<string[]>(DEFAULTCONTRACTORS)
+  const [event, setEvent] = useState<EventType>({name: '', type: ''})
 
   const handleActiveStages = (stage: number) => setActiveStage(++stage)
 
@@ -20,30 +24,19 @@ export default function Host() {
       <Text size='SMALL'>Host An Event</Text>
       <div className='flex flex-col gap-4 justify-start items-start w-fit mx-auto mt-16'>
         {activeStage === 1 && (
-          <ChooseEventType
-            setEventName={setEventName}
-            setEventType={setEventType}
-            validated={!(eventName && eventType)}
+          <DefineEvent
+            setEventName={(name: string) => setEvent(prev => ({...prev, name}))}
+            setEventType={(type: string) => setEvent(prev => ({...prev, type}))}
+            validated={!(event.name && event.type)}
             onNext={() => handleActiveStages(1)}
           />
-        )}
-        {activeStage === 2 && (
-          <ChooseContractorTypes
-            contractorTypes={contractorTypes}
-            setContractorTypes={setContractorTypes}
-            validated={contractorTypes.length === 0}
-            onNext={() => handleActiveStages(2)}
-          />
-        )}
-        {activeStage === 3 && (
-          <SelectContractors contractorTypes={contractorTypes} />
         )}
       </div>
     </div>
   )
 }
 
-function ChooseEventType({
+function DefineEvent({
   setEventName,
   setEventType,
   validated,
@@ -92,102 +85,5 @@ function ChooseEventType({
         Next
       </Button>
     </>
-  )
-}
-
-function ChooseContractorTypes({
-  contractorTypes,
-  setContractorTypes,
-  validated,
-  onNext,
-}: {
-  contractorTypes: string[]
-  setContractorTypes: (type: (prevTypes: string[]) => string[]) => void
-  validated: boolean
-  onNext: () => void
-}) {
-  const [newContractorType, setNewContractorType] = useState('')
-
-  const handleAdd = () =>
-    setContractorTypes((prevTypes) => [...prevTypes, newContractorType])
-
-  const handleRemove = (id: number) => {
-    setContractorTypes((prevTypes) => prevTypes.filter((_, _id) => _id !== id))
-  }
-
-  return (
-    <>
-      <Text size='BOLD' className='text-center mb-10'>
-        EVENT CONTRACTORS
-      </Text>
-      <div className='flex justify-start items-start gap-3 w-full mb-5'>
-        <input
-          onChange={(e) => setNewContractorType(e.target.value)}
-          required
-          type='text'
-          name='contractortype'
-          id='contractortype'
-          placeholder='Contractor Type'
-          className='flex-grow border border-zinc-600/50 rounded-xl p-2 focus:outline-blue-500/50 w-full'
-        />
-        <Button
-          type='button'
-          className='disabled:bg-gray-600/50 disabled:border-gray-600/50'
-          onClick={handleAdd}
-        >
-          Add
-        </Button>
-      </div>
-      <div className='flex flex-wrap justify-start items-center gap-3'>
-        {contractorTypes.map((type, id) => (
-          <div className='flex justify-start items-center gap-7 bg-blue-600/20 p-2 rounded-xl'>
-            <Text size='SMALL'>{type}</Text>
-            <Button
-              type='button'
-              className='bg-transparent px-0 py-0 w-6 h-6 flex justify-center items-center rounded-full border-red-600'
-              onClick={() => handleRemove(id)}
-            >
-              <MdClose className='text-base text-red-600' />
-            </Button>
-          </div>
-        ))}
-      </div>
-      <Button
-        type='button'
-        disabled={validated}
-        className='disabled:bg-gray-600/50 disabled:border-gray-600/50 mt-5'
-        onClick={onNext}
-      >
-        Next
-      </Button>
-    </>
-  )
-}
-
-function SelectContractors({ contractorTypes }: { contractorTypes: string[] }) {
-  const contractors: { name: string; role: string; pay: string }[] = [
-    { name: 'Obi CUbana', role: 'Structural', pay: '$30/hr' },
-    { name: 'Nneoma James', role: 'Cook', pay: '$10/hr' },
-    { name: 'Amadi Johnson', role: 'Dancer', pay: '$5/hr' },
-    { name: 'Juliet Ann', role: 'MC', pay: '$7/hr' },
-  ]
-
-  return (
-    <div className='overflow-scroll'>
-      <select name='contractorTypes'>
-        {contractorTypes.map((type) => (
-          <option value={type}>{type}</option>
-        ))}
-      </select>
-      {contractors.map(({ name, role, pay }) => (
-        <div className='p-3 rounded-xl border border-gray-900/20 bg-blue-600/30'>
-          <div className='flex justify-between items-center gap-10'>
-            <span>{name}</span>
-            <span>{pay}</span>
-          </div>
-          <span>{role}</span>
-        </div>
-      ))}
-    </div>
   )
 }
