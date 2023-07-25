218eb380d1fd90267dcc04cee5d77148a28efc93
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-25 17:26:36 +0100

    Fix: Format with prettier

--- app/dashboard/events/host/page.tsx
+++ app/dashboard/events/host/page.tsx
@@ -98,38 +98,47 @@ type ContractorType = {
   status: boolean
 }
 
-function ContractorSelection({validated, onNext}: {validated: boolean, onNext: () => void}) {
+function ContractorSelection({
+  validated,
+  onNext,
+}: {
+  validated: boolean
+  onNext: () => void
+}) {
   const [searchText, setSearchText] = useState<string>('')
   const [filter, setFilter] = useState<string>('name')
 
   return (
     <>
-    <div className='flex justify-center items-center gap-3'>
-      <input
-      onChange={(e) => setSearchText(e.target.value)}
-      required
-      type='text'
-      name='searchText'
-      id='searchText'
-      placeholder='Emeka or Dancer or $50/hr'
-      className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50 w-full'
-    />
-    <select name='filter' defaultValue={filter} onChange={(e) => setFilter(e.target.value)}>
-      <option value='category'>category</option>
-      <option value='role'>role</option>
-      <option value='name'>name</option>
-      <option value='pay'>pay</option>
-    </select>
-    </div>
-    <Button
-      type='button'
-      disabled={validated}
-      className='disabled:bg-gray-600/50 disabled:border-gray-600/50'
-      onClick={onNext}
-    >
-      Next
-    </Button>
+      <div className='flex justify-center items-center gap-3'>
+        <input
+          onChange={(e) => setSearchText(e.target.value)}
+          required
+          type='text'
+          name='searchText'
+          id='searchText'
+          placeholder='Emeka or Dancer or $50/hr'
+          className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50 w-full'
+        />
+        <select
+          name='filter'
+          defaultValue={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value='category'>category</option>
+          <option value='role'>role</option>
+          <option value='name'>name</option>
+          <option value='pay'>pay</option>
+        </select>
+      </div>
+      <Button
+        type='button'
+        disabled={validated}
+        className='disabled:bg-gray-600/50 disabled:border-gray-600/50'
+        onClick={onNext}
+      >
+        Next
+      </Button>
     </>
   )
 }
-
