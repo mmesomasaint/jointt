a874b5f2cccd44ab45629b9a336b9f65b815034c
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-29 19:22:15 +0100

    Fix: Format with pretter

--- app/dashboard/getQuery.ts
+++ app/dashboard/getQuery.ts
@@ -1,11 +1,24 @@
 import firebase_app from '../firebase'
-import { getFirestore, collection, getDocs, query, where, WhereFilterOp, FieldPath } from 'firebase/firestore'
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+  WhereFilterOp,
+  FieldPath,
+} from 'firebase/firestore'
 
 const db = getFirestore(firebase_app)
 
-export default async function getDataWithQuery(collectionName: string, fieldpath: string | FieldPath, op: WhereFilterOp, value: unknown) {
-  const collectionRef = collection(db, collectionName);
-  const q = query(collectionRef, where(fieldpath, op, value));
+export default async function getDataWithQuery(
+  collectionName: string,
+  fieldpath: string | FieldPath,
+  op: WhereFilterOp,
+  value: unknown
+) {
+  const collectionRef = collection(db, collectionName)
+  const q = query(collectionRef, where(fieldpath, op, value))
   let result = null
   let error = null
 
