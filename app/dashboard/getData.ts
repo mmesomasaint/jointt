02265ed8b1501e1bd96792b2fa0ea294e9cf3d7f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-29 13:45:42 +0100

    Fix: Format with pretter

--- app/dashboard/getData.ts
+++ app/dashboard/getData.ts
@@ -1,18 +1,19 @@
-import firebase_app from "../firebase";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import firebase_app from '../firebase'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
 
 const db = getFirestore(firebase_app)
+
 export default async function getDoument(collection: string, id: string) {
-    let docRef = doc(db, collection, id);
+  let docRef = doc(db, collection, id)
 
-    let result = null;
-    let error = null;
+  let result = null
+  let error = null
 
-    try {
-        result = await getDoc(docRef);
-    } catch (e) {
-        error = e;
-    }
+  try {
+    result = await getDoc(docRef)
+  } catch (e) {
+    error = e
+  }
 
-    return { result, error };
+  return { result, error }
 }
