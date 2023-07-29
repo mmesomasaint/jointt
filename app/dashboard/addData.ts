1be9fa230ca7e8a3bd4db7465095f1cad9f69b60
Mmesoma Saint <mmesomasaint@gmail.com>
2023-07-29 13:39:20 +0100

    Fix: Format with pretter

--- app/dashboard/addData.ts
+++ app/dashboard/addData.ts
@@ -1,16 +1,16 @@
-import firebase_app from "../firebase";
-import { getFirestore, addDoc, collection } from "firebase/firestore";
+import firebase_app from '../firebase'
+import { getFirestore, addDoc, collection } from 'firebase/firestore'
 
 const db = getFirestore(firebase_app)
 export default async function addData(collectionName: string, data: object) {
-    let result = null;
-    let error = null;
+  let result = null
+  let error = null
 
-    try {
-        result = await addDoc(collection(db, collectionName), data);;
-    } catch (e) {
-        error = e;
-    }
+  try {
+    result = await addDoc(collection(db, collectionName), data)
+  } catch (e) {
+    error = e
+  }
 
-    return { result, error };
+  return { result, error }
 }
