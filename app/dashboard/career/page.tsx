f30145495bf5aaed0b0ffa0f4621d59d70bc7c63
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-01 14:14:56 +0100

    Feat: Setup other fields

--- app/dashboard/career/page.tsx
+++ app/dashboard/career/page.tsx
@@ -138,6 +138,47 @@ function ActivationForm({
               className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50 w-full'
             />
           </label>
+          <div className='flex justify-evenly items-center gap-5'>
+          <label htmlFor='role' className='w-full'>
+            <Text size='SMALL'>Role</Text>
+            <input
+              onChange={(e) => setRole(e.target.value)}
+              required
+              type='text'
+              name='role'
+              id='role'
+              value={role}
+              placeholder='Dancer'
+              className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50 w-full'
+            />
+          </label>
+          <label htmlFor='pay' className='w-full'>
+            <Text size='SMALL'>Pay</Text>
+            $
+            <input
+              onChange={(e) => setPay(e.target.value)}
+              required
+              type='text'
+              name='pay'
+              id='pay'
+              value={pay}
+              placeholder='50'
+              className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50 w-full'
+            />/hr
+          </label>
+          <label htmlFor='status' className='w-full'>
+            <Text size='SMALL'>Status</Text>
+            <input
+              onChange={(e) => setStatus(e.target.value)}
+              required
+              type='checkbox'
+              name='status'
+              id='status'
+              value={status}
+              className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50 w-full'
+            />
+          </label>
+          </div>
         </form>
       </Modal>
     </>
