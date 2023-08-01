0bbc8786be8c6069607b26301d26096c8592c231
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-01 16:12:36 +0100

    Fix: Format with prettier

--- app/dashboard/career/components/jobCard.tsx
+++ app/dashboard/career/components/jobCard.tsx
@@ -1,25 +1,37 @@
-import Text from "@/app/components/text";
+import Text from '@/app/components/text'
 
-export default function JobCard({title, desc, roles, budget, expireDate}: {title: string, desc: string, roles: string[], budget: number, expireDate: number}) {
+export default function JobCard({
+  title,
+  desc,
+  roles,
+  budget,
+  expireDate,
+}: {
+  title: string
+  desc: string
+  roles: string[]
+  budget: number
+  expireDate: number
+}) {
   return (
-    <div className="rounded-xl bg-white text-black p-5">
-      <Text size="SEMIBOLD">{title}</Text>
-      <Text size="SMALL">{desc}</Text>
-      <div className="flex justify-start items-center gap-3">
-        {roles.map(role => (
-          <div key={role} className="rounded-xl bg-gray-900/50 p-2 shadow-xl">
-            <Text size="SMALL">{role}</Text>
+    <div className='rounded-xl bg-white text-black p-5'>
+      <Text size='SEMIBOLD'>{title}</Text>
+      <Text size='SMALL'>{desc}</Text>
+      <div className='flex justify-start items-center gap-3'>
+        {roles.map((role) => (
+          <div key={role} className='rounded-xl bg-gray-900/50 p-2 shadow-xl'>
+            <Text size='SMALL'>{role}</Text>
           </div>
         ))}
       </div>
-      <div className="flex justify-between items-center gap-10">
-        <div className="flex justify-start items-center gap-1">
-          <Text size="SMALL">Budge:</Text>
-          <Text size="SMALL">{budget}</Text>
+      <div className='flex justify-between items-center gap-10'>
+        <div className='flex justify-start items-center gap-1'>
+          <Text size='SMALL'>Budge:</Text>
+          <Text size='SMALL'>{budget}</Text>
         </div>
-        <div className="flex justify-start items-center gap-1">
-          <Text size="SMALL">Expire:</Text>
-          <Text size="SMALL">{new Date(expireDate).toLocaleDateString()}</Text>
+        <div className='flex justify-start items-center gap-1'>
+          <Text size='SMALL'>Expire:</Text>
+          <Text size='SMALL'>{new Date(expireDate).toLocaleDateString()}</Text>
         </div>
       </div>
     </div>
