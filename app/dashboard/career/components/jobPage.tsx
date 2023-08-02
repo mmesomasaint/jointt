becf506c8a51ec3b4762b03a01f7863827e22881
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-02 20:02:51 +0100

    Fix: Format with prettier

--- app/dashboard/career/components/jobPage.tsx
+++ app/dashboard/career/components/jobPage.tsx
@@ -1,6 +1,6 @@
-import Button from "@/app/components/button";
-import Text from "@/app/components/text";
-import Modal from "../../components/modal";
+import Button from '@/app/components/button'
+import Text from '@/app/components/text'
+import Modal from '../../components/modal'
 
 export default function JobPage({
   title,
@@ -8,7 +8,7 @@ export default function JobPage({
   roles,
   budget,
   expireDate,
-  created
+  created,
 }: {
   title: string
   desc: string
@@ -20,29 +20,31 @@ export default function JobPage({
   return (
     <div className='rounded-xl bg-gray-600/10 text-black p-5 border border-gray-600/50 w-[70%] focus:bg-blue-600/10'>
       <Text size='SEMIBOLD'>{title}</Text>
-      <div className="flex justify-start items-center gap-1">
-      <Text size="SMALL" className="text-gray-900/70">Posted:</Text>
-      <Text size="SMALL">{created}</Text>
+      <div className='flex justify-start items-center gap-1'>
+        <Text size='SMALL' className='text-gray-900/70'>
+          Posted:
+        </Text>
+        <Text size='SMALL'>{created}</Text>
       </div>
-        <div className='flex justify-start items-center gap-1'>
-          <Text size='SMALL'>Expire:</Text>
-          <Text size='SMALL'>{new Date(expireDate).toLocaleDateString()}</Text>
-        </div>
-        <hr className='bg-black/50' />
-        <div className="flex flex-col gap-2">
-        <Text size="SMALL">Description:&nbsp;</Text>
-        <Text size="SMALL">{desc}</Text>
-        </div>
-        <hr className="bg-black/50" />
-        <div className="flex flex-col gap-2">
-          <Text size="SMALL">Roles:&nbsp;</Text>
-      <div className='flex justify-start items-center gap-3'>
-        {roles.map((role) => (
-          <div key={role} className='rounded-xl bg-gray-900/50 p-2 shadow-xl'>
-            <Text size='SMALL'>{role}</Text>
-          </div>
-        ))}
+      <div className='flex justify-start items-center gap-1'>
+        <Text size='SMALL'>Expire:</Text>
+        <Text size='SMALL'>{new Date(expireDate).toLocaleDateString()}</Text>
       </div>
+      <hr className='bg-black/50' />
+      <div className='flex flex-col gap-2'>
+        <Text size='SMALL'>Description:&nbsp;</Text>
+        <Text size='SMALL'>{desc}</Text>
+      </div>
+      <hr className='bg-black/50' />
+      <div className='flex flex-col gap-2'>
+        <Text size='SMALL'>Roles:&nbsp;</Text>
+        <div className='flex justify-start items-center gap-3'>
+          {roles.map((role) => (
+            <div key={role} className='rounded-xl bg-gray-900/50 p-2 shadow-xl'>
+              <Text size='SMALL'>{role}</Text>
+            </div>
+          ))}
+        </div>
       </div>
       <div className='flex justify-between items-center gap-10'>
         <div className='flex justify-start items-center gap-1'>
@@ -50,7 +52,9 @@ export default function JobPage({
           <Text size='SMALL'>{budget}</Text>
         </div>
       </div>
-      <Button type="button" className="mt-4">Apply Now</Button>
+      <Button type='button' className='mt-4'>
+        Apply Now
+      </Button>
     </div>
   )
 }
