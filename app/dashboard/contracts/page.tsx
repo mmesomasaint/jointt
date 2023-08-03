7feb52199db3c4c7810c3d960c306570b3ae6c54
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-03 02:51:35 +0100

    Fix: Create a mock data and test the contractCard

--- app/dashboard/contracts/page.tsx
+++ app/dashboard/contracts/page.tsx
@@ -4,6 +4,51 @@ import { useState } from 'react'
 import Text from '@/app/components/text'
 import Tab from '../components/tab'
 import NoContent from '../components/nocontent'
+import ContractCard from './component/contractCard'
+
+type ContractType = {
+  title: string
+  role: string
+  host: string
+  recipient: string
+  startDate: number
+  endDate: number
+}
+
+const contracts: ContractType[] = [
+  {
+    title: "Birthday Party For MY 3yo Daughter.",
+    role: "Dancer",
+    host: "Dr Mahatma Gaundi",
+    recipient: "James Gunn",
+    startDate: new Date(Date.parse("07/09/2023")).getTime(),
+    endDate: new Date(Date.parse("08/09/2023")).getTime(),
+  },
+  {
+    title: "Birthday Party For MY 3yo Daughter.",
+    role: "Dancer",
+    host: "Dr Mahatma Gaundi",
+    recipient: "James Gunn",
+    startDate: new Date(Date.parse("07/09/2023")).getTime(),
+    endDate: new Date(Date.parse("08/09/2023")).getTime(),
+  },
+  {
+    title: "Birthday Party For MY 3yo Daughter.",
+    role: "Dancer",
+    host: "Dr Mahatma Gaundi",
+    recipient: "James Gunn",
+    startDate: new Date(Date.parse("07/09/2023")).getTime(),
+    endDate: new Date(Date.parse("08/09/2023")).getTime(),
+  },
+  {
+    title: "Birthday Party For MY 3yo Daughter.",
+    role: "Dancer",
+    host: "Dr Mahatma Gaundi",
+    recipient: "James Gunn",
+    startDate: new Date(Date.parse("07/09/2023")).getTime(),
+    endDate: new Date(Date.parse("08/09/2023")).getTime(),
+  },
+]
 
 export default function Contracts() {
   const [activeTab, setActiveTab] = useState<string>('Host')
@@ -32,7 +77,7 @@ export default function Contracts() {
         </div>
         <hr className='bg-black/50' />
         <div className='flex-grow p-3 flex flex-col justify-start items-center gap-3 overflow-auto'>
-          <NoContent />
+          {contracts.length > 0 ? contracts.map(contract => <ContractCard title={contract.title} role={contract.role} host={contract.host} recipient={contract.recipient} startDate={contract.startDate} endDate={contract.endDate} />) : <NoContent />}
         </div>
       </div>
     </>
