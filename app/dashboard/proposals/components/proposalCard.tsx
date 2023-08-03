42132f01495f592fef870931a2c029f35351ac92
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-03 23:27:41 +0100

    Feat: Create param

--- app/dashboard/proposals/components/proposalCard.tsx
+++ app/dashboard/proposals/components/proposalCard.tsx
@@ -1,26 +1,26 @@
 import Button from '@/app/components/button'
 import Text from '@/app/components/text'
 
-export default function ProposalCard() {
+export default function ProposalCard({from, jobTitle, text, created}: {from: string, jobTitle: string, text: string, created: number}) {
   return (
     <div className='w-[70%] bg-gray-600/10 shadow-md p-5 rounded-xl'>
       <div className='flex justify-between items-center gap-10'>
         <div className='flex justify-start items-center gap-2'>
           <Text size='SMALL'>From:</Text>
-          <Text size='SMALL'>Dr. R. Didiumini</Text>
+          <Text size='SMALL'>{from}</Text>
         </div>
         <div className='flex justify-start items-center gap-2'>
           <Text size='MEDIUM'>Date:</Text>
-          <Text size='MEDIUM'>7/11/2023</Text>
+          <Text size='MEDIUM'>{new Date(created).toLocaleDateString()}</Text>
         </div>
       </div>
       <div className='flex justify-start items-center gap-2'>
         <Text size='MEDIUM'>For:</Text>
-        <Text size='MEDIUM'>Birthday Party For My 3yo Daughter.</Text>
+        <Text size='MEDIUM'>{jobTitle}</Text>
       </div>
       <div className='flex flex-col gap-3'>
         <Text size='SMALL'>Proposal:</Text>
-        <Text size='SMALL'>Lorem ipsum dolor...</Text>
+        <Text size='SMALL'>{text}</Text>
         <Button type='button'>Read More...</Button>
       </div>
     </div>
