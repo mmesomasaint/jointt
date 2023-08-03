fd7eb45563a46dc1915e18ca71812c5e261c43e8
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-03 02:43:16 +0100

    Feat: Add params and types

--- app/dashboard/contracts/component/contractCard.tsx
+++ app/dashboard/contracts/component/contractCard.tsx
@@ -1,33 +1,33 @@
 import Text from '@/app/components/text'
 
-export default function ContractCard() {
+export default function ContractCard({title, role, host, recipient, startDate, endDate}: {title: string, role: string, host: string, recipient: string, startDate: number, endDate: number}) {
   return (
     <div className='bg-gray-600/10 border border-gray-600/50 shadow-xl p-5 rounded-xl'>
       <div className='flex flex-col gap-3'>
         <div className='flex justify-between items-center gap-10'>
           <div className='flex justify-start items-center gap-2'>
             <Text size='SMALL'>Host:</Text>
-            <Text size='SMALL'>Dr. Amadi DD</Text>
+            <Text size='SMALL'>{host}</Text>
           </div>
           <div className='flex justify-start items-center gap-2'>
             <Text size='SMALL'>Recipient:</Text>
-            <Text size='SMALL'>James Gunn</Text>
+            <Text size='SMALL'>{recipient}</Text>
           </div>
         </div>
-        <Text size='MEDIUM'>Birthday Party For My 3yo Daughter.</Text>
+        <Text size='MEDIUM'>{title}</Text>
         <div className='flex justify-between items-center gap-10'>
           <div className='flex justify-start items-center gap-2'>
             <Text size='SMALL'>Role:</Text>
-            <Text size='SMALL'>Dancer</Text>
+            <Text size='SMALL'>{role}</Text>
           </div>
           <div className='flex justify-evenly items-center gap-2'>
             <div className='flex justify-start items-center gap-2'>
               <Text size='SMALL'>From:</Text>
-              <Text size='SMALL'>7/06/2023</Text>
+              <Text size='SMALL'>{new Date(startDate).toLocaleDateString()}</Text>
             </div>
             <div className='flex justify-start items-center gap-2'>
               <Text size='SMALL'>To:</Text>
-              <Text size='SMALL'>7/07/2023</Text>
+              <Text size='SMALL'>{new Date(endDate).toLocaleDateString()}</Text>
             </div>
           </div>
         </div>
