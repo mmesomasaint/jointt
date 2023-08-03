78b03dec452827cf438ccb8b2dd54ea84320c583
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-03 02:43:25 +0100

    Fix: Format with prettier

--- app/dashboard/contracts/component/contractCard.tsx
+++ app/dashboard/contracts/component/contractCard.tsx
@@ -1,6 +1,20 @@
 import Text from '@/app/components/text'
 
-export default function ContractCard({title, role, host, recipient, startDate, endDate}: {title: string, role: string, host: string, recipient: string, startDate: number, endDate: number}) {
+export default function ContractCard({
+  title,
+  role,
+  host,
+  recipient,
+  startDate,
+  endDate,
+}: {
+  title: string
+  role: string
+  host: string
+  recipient: string
+  startDate: number
+  endDate: number
+}) {
   return (
     <div className='bg-gray-600/10 border border-gray-600/50 shadow-xl p-5 rounded-xl'>
       <div className='flex flex-col gap-3'>
@@ -23,7 +37,9 @@ export default function ContractCard({title, role, host, recipient, startDate, e
           <div className='flex justify-evenly items-center gap-2'>
             <div className='flex justify-start items-center gap-2'>
               <Text size='SMALL'>From:</Text>
-              <Text size='SMALL'>{new Date(startDate).toLocaleDateString()}</Text>
+              <Text size='SMALL'>
+                {new Date(startDate).toLocaleDateString()}
+              </Text>
             </div>
             <div className='flex justify-start items-center gap-2'>
               <Text size='SMALL'>To:</Text>
