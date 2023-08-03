8a1e57394abe0293b76dfb542492deefe28cda57
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-03 23:18:46 +0100

    Fix: Format with prettier

--- app/dashboard/proposals/components/proposalCard.tsx
+++ app/dashboard/proposals/components/proposalCard.tsx
@@ -1,28 +1,28 @@
-import Button from "@/app/components/button";
-import Text from "@/app/components/text";
+import Button from '@/app/components/button'
+import Text from '@/app/components/text'
 
 export default function ProposalCard() {
   return (
     <div className='w-[70%] bg-gray-600/10 shadow-md p-5 rounded-xl'>
-      <div className="flex justify-between items-center gap-10">
-    <div className='flex justify-start items-center gap-2'>
-      <Text size='SMALL'>By:</Text>
-      <Text size='SMALL'>Dr. R. Didiumini</Text>
-    </div>
-    <div className='flex justify-start items-center gap-2'>
-      <Text size='MEDIUM'>Date:</Text>
-      <Text size='MEDIUM'>7/11/2023</Text>
-    </div>
-    </div>
-    <div className='flex justify-start items-center gap-2'>
-      <Text size='MEDIUM'>For:</Text>
-      <Text size='MEDIUM'>Birthday Party For My 3yo Daughter.</Text>
-    </div>
-    <div className="flex flex-col gap-3">
-      <Text size="SMALL">Proposal:</Text>
-      <Text size="SMALL">Lorem ipsum dolor...</Text>
-      <Button type="button">Read More...</Button>
-    </div>
+      <div className='flex justify-between items-center gap-10'>
+        <div className='flex justify-start items-center gap-2'>
+          <Text size='SMALL'>By:</Text>
+          <Text size='SMALL'>Dr. R. Didiumini</Text>
+        </div>
+        <div className='flex justify-start items-center gap-2'>
+          <Text size='MEDIUM'>Date:</Text>
+          <Text size='MEDIUM'>7/11/2023</Text>
+        </div>
+      </div>
+      <div className='flex justify-start items-center gap-2'>
+        <Text size='MEDIUM'>For:</Text>
+        <Text size='MEDIUM'>Birthday Party For My 3yo Daughter.</Text>
+      </div>
+      <div className='flex flex-col gap-3'>
+        <Text size='SMALL'>Proposal:</Text>
+        <Text size='SMALL'>Lorem ipsum dolor...</Text>
+        <Button type='button'>Read More...</Button>
+      </div>
     </div>
   )
 }
