ddeeddf660cc021db3f8d85db919edc966d0213f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-04 22:27:47 +0100

    Fix: Scroll automatically

--- app/dashboard/events/page.tsx
+++ app/dashboard/events/page.tsx
@@ -25,7 +25,7 @@ export default function Events() {
         </div>
       </div>
       <hr className='bg-black/50' />
-      <div className='flex-grow p-3 flex flex-col justify-start items-center gap-3 overflow-scroll'>
+      <div className='flex-grow p-3 flex flex-col justify-start items-center gap-3 overflow-y-auto'>
         {events.length === 0 ? (
           <NoContent />
         ) : (
