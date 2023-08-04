4bf5e405e7e2b8b66443d401f78bbda2d8ee36ae
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-04 23:30:58 +0100

    Fix: Format with prettier

--- app/dashboard/components/tab.tsx
+++ app/dashboard/components/tab.tsx
@@ -1,20 +1,20 @@
 'use client'
 
-import {useState} from 'react'
+import { useState } from 'react'
 
-export default function TabHandler({titles}: {titles: string[]}) {
+export default function TabHandler({ titles }: { titles: string[] }) {
   const [activeTab, setActiveTab] = useState<string>('')
 
   return (
     <>
-      {titles.map(title => (
-          <Tab
+      {titles.map((title) => (
+        <Tab
           key={title}
-            onClick={() => setActiveTab(title)}
-            active={activeTab === title}
-          >
-            {title}
-          </Tab>
+          onClick={() => setActiveTab(title)}
+          active={activeTab === title}
+        >
+          {title}
+        </Tab>
       ))}
     </>
   )
@@ -32,7 +32,9 @@ function Tab({
   return (
     <button
       type='button'
-      className={`${active ? 'bg-blue-600 text-white' : 'bg-white text-black'} text-black/70 text-xl font-medium leading-none`}
+      className={`${
+        active ? 'bg-blue-600 text-white' : 'bg-white text-black'
+      } text-black/70 text-xl font-medium leading-none`}
       onClick={onClick}
     >
       {children}
