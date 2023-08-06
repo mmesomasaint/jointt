146cb8c0d3c672d6157e00137366bfb3db90f507
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-06 06:26:42 +0100

    Feat: Design the useCareerProfile hook

---
+++ app/dashboard/career/components/usecareerprofile.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { DocumentData } from "firebase/firestore";
+import getDataWithQuery from "../../getQuery";
+import { useAuthContext } from "@/app/auth/authcontext";
+
+export default function useCareerProfile(userId: string | null) {
+  const [careerProfile, setCareerProfile] = useState<DocumentData>()
+  const {user} = useAuthContext()
+
+  const getUserCareerData = async () => {
+    const { result, error } = await getDataWithQuery(
+      'career_profiles',
+      'userId',
+      '==',
+      userId
+    )
+    if (error) {
+      console.log('User Career Error: \n', error)
+      return null
+    }
+    return result?.docs[0]
+  }
+
+  
+
+  useEffect(() => {
+    const isCareerActive = async () => {
+      const userData = await getUserCareerData()
+      if (userData) {
+        setCareerProfile(userData)
+      }
+    }
+
+    isCareerActive()
+  }, [])
+
+  return {careerProfile}
+}
